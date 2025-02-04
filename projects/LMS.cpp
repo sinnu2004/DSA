60e6df3337e541fc556fe28f3a903361c8050032
sinnu2004 <prajapatsaurabh190@gmail.com>
2025-02-04 12:49:32 +0530

    some changes

--- projects/LMS.cpp
+++ projects/LMS.cpp
@@ -1,50 +1,74 @@
 #include<iostream>
 #include<vector>
-#include<stack>
+#include<map>
+#include<unordered_map>
 #include<queue>
-#include<string>
+#include<stack>
 using namespace std;
-class Node{
-    public: 
-        Node* next;
-        int val;
-        Node(int val){
-            this->next = NULL;
-            this->val = val; 
+void add_book(vector<string> &book,string b_name){
+    book.push_back(b_name);
+}
+void search_book(vector<string> &book,string f_name){
+    int lo = 0;
+    int hi = book.size()-1;
+    string x = "python";
+    while(lo<=hi){
+        int mid = (hi+lo)/2;
+        if(book[mid]==x) {
+            cout<<"found";
+            break;
         }
-};
-class linked{
-    public:
-        Node* head;
-        Node* tail;
-        int size;
-        linked(){
-            head = tail = NULL;
-            size = 0;
-        }
-    public:
-        void add(int val){
-            Node* temp = new Node(val);
-            if(size==0) head = tail = temp;
-            else {
-                tail->next = temp;
-                temp = temp->next;
-            }
-        }
-        void display(){
-            Node* temp = head;
-            while(temp!=NULL){
-                cout<<temp->val<<" ";
-                temp = temp->next;
-            }
-            cout<<endl;
-        }
-};  
-int maint(){
-    // linked ll;
-    // ll.add(10);
-    // ll.add(11);
-    // ll.add(12);
-    // ll.display()
-    cout<<"hello world";
+        else if(book[mid]>x) lo = mid + 1;
+        else hi = mid - 1;
+    }
+    cout<<endl;
+}
+void all_book(vector<string>& book){
+    for(int i=0;i<book.size();i++){
+        cout<<book[i]<<" ";
+    }
+    cout<<endl;
+}
+void alloted_request(queue<string>& q,string r_name){
+    q.push(r_name);
+}
+void request_history(queue<string>& q){
+    while(q.size()>0){
+        cout<<q.front()<<" ";
+        q.pop();
+    }
+    cout<<endl;
+}
+void add_student(vector<string>& student,string st_name){
+    student.push_back(st_name);
+}
+void st_request_history(queue<int>& book_id,int b_id){
+    book_id.push(b_id);
+}
+void st_history(queue<int>& book_id){
+    while(book_id.size()>0){
+        cout<<book_id.front()<<" ";
+        book_id.pop();
+    }
+    cout<<endl;
+}
+int main(){
+    vector<string> book;
+    add_book(book,"python");   // for adding a new books 
+    add_book(book,"c++");
+    add_book(book,"java");
+    search_book(book,"java");  // for searching a books 
+    all_book(book);             // for printing all available books
+    queue<string> q;   
+    string r_name = "java";
+    alloted_request(q,r_name);  // pull request for book allotments
+    request_history(q);         // request history of books
+
+    vector<string> student;
+    queue<int> book_id;
+    add_student(student,"saurabh");   // adding new students
+    st_request_history(book_id,123);  // students pull request for book allotment
+    st_history(book_id);               // book request history
+
+    
 }
